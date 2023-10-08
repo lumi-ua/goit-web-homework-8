77ea7d652eef505f133ff4890c240bdeb200a134
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-10-08 21:16:09 +0300

    Update produser.py

--- hw2/produser.py
+++ hw2/produser.py
@@ -30,7 +30,7 @@ for contact_data in contacts:
     channel.basic_publish(
     exchange='',
     routing_key='email_queue',
-    body=str(contact.id).encode(), #здесь записываем либо в джсон либо в монго бд или постгрес іd объекта
+    body=str(contact.id).encode(), 
     properties=pika.BasicProperties(
         delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
     ))
